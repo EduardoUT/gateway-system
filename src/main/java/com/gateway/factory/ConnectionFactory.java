7c4561a40484764638f92def25bce986d9f5ef65
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-10-06 18:22:18 -0500

    Agregando clase para pool de conexiones con la BD

---
+++ src/main/java/com/gateway/factory/ConnectionFactory.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gateway.factory;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
+/**
+ *
+ * Utilizando patrón de diseño Factory para crear un pool de conexiones de
+ * máximo 10.
+ *
+ * Al ser utilizada esta clase en distintos puntos del proyecto, para conectarse
+ * a la base de datos.
+ *
+ * @author Eduardo Reyes Hernández
+ */
+public class ConnectionFactory {
+
+    private final DataSource dataSource;
+
+    public ConnectionFactory() {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/gateway?"
+                + "useTimeZone=true&serverTimeZone=UTC");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("root");
+        comboPooledDataSource.setMaxPoolSize(10);
+        this.dataSource = comboPooledDataSource;
+    }
+
+    public Connection realizarConexion() {
+        try {
+            return this.dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
