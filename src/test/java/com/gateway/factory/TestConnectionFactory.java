7c34b95a3b1733a0036f08c07e6488feef8b561e
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-10-06 18:31:10 -0500

    Agregando paquete de pruebas

---
+++ src/test/java/com/gateway/factory/TestConnectionFactory.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gateway.factory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Eduardo Reyes Hernández
+ */
+public class TestConnectionFactory {
+
+    public static void main(String[] args) {
+        try {
+            Connection con = new ConnectionFactory().realizarConexion();
+            con.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
