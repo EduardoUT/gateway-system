f81a081c3d6f8e43a6d2623178c334874ef0c6a4
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-10-14 14:40:04 -0500

    Clase de prueba de claves

---
+++ src/main/java/com/mx/grupogateway/system/security/TestKeyGenerator.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mx.grupogateway.system.security;
+
+import com.mx.grupogateway.system.modelo.Usuario;
+
+/**
+ *
+ * @author Eduardo Reyes Hernández
+ */
+public class TestKeyGenerator {
+
+    public static void main(String[] args) {
+        /*
+        Usuario u = new Usuario("Admin", "riverdale21");
+        System.out.println(u.getClaveSeguridad());
+        Usuario us = new Usuario("User12", "pasteur");
+        System.out.println(us.getClaveSeguridad());
+        Usuario use = new Usuario("randomiuser", "micaedrous");
+        Usuario pe = new Usuario("plateur", "espiralmar34");
+        System.out.println(use.getClaveSeguridad());
+        System.out.println(pe.getClaveSeguridad());
+         */
+ /*
+        ProtectorKey passwordSegura = new ProtectorKey("ac33-2177c62e02b");
+        ProtectorKey passwordSegura1 = new ProtectorKey("b762-d04a2c9d2d1");
+        ProtectorKey passwordSegura2 = new ProtectorKey("abcd-a4a322ead02");
+        ProtectorKey passwordSegura3 = new ProtectorKey("b8bd-b3165faa6b4");
+
+        System.out.println(passwordSegura.encriptarString());
+        System.out.println(passwordSegura1.encriptarString());
+        System.out.println(passwordSegura2.encriptarString());
+        System.out.println(passwordSegura3.encriptarString());
+         */
+
+    }
+}
