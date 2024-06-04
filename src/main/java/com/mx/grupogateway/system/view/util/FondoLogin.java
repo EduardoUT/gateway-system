747b3ff056ec5c8ccfc8911aee1cf5ae9fb592ce
eduar <mcorey316@gmail.com>
2024-06-03 22:29:13 -0600

    Agregando JFrame con imagen de fondo

---
+++ src/main/java/com/mx/grupogateway/system/view/util/FondoLogin.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mx.grupogateway.system.view.util;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author eduar
+ */
+public class FondoLogin extends JPanel {
+    
+    private Image imagen;
+
+        @Override
+        public void paint(Graphics g) {
+            imagen = new ImageIcon(getClass().getResource("/Imagenes/FondoLogin.jpg")).getImage();
+            g.drawImage(imagen, 0, 0, getWidth(), getHeight(), this);
+            setOpaque(false);
+            super.paint(g);
+        }
+}
