ad096d3b12df9fc9bd275561912fbfbcff385d80
eduardo <mcorey316@gmail.com>
2024-06-13 14:11:00 -0600

    Definiendo encabezados de tabla de Proyectos

---
+++ src/main/java/com/mx/grupogateway/system/util/IdentificadoresProyecto.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.mx.grupogateway.system.util;
+
+/**
+ *
+ * @author eduar
+ */
+public enum IdentificadoresProyecto {
+    ID,
+    PO_NO,
+    PO_STATUS,
+    PO_LINE_NO,
+    PROJECT_CODE,
+    PROJECT_NAME,
+    CUSTOMER,
+    SHIPMENT_NO,
+    SITE_CODE,
+    SITE_NAME,
+    ITEM_CODE,
+    ITEM_DESCRIPTION,
+    REQUESTED_QTY,
+    DUE_QTY,
+    BILLED_QTY,
+    UNIT_PRICE,
+    LINE_AMOUNT,
+    UNIT,
+    PAYMENT_TERMS,
+    CATEGORY,
+    BIDDING_AREA,
+    PUBLISH_DATE;
+}
+
