9009f2e46adb49c096702b799715201012711bcf
eduardo <mcorey316@gmail.com>
2024-06-13 14:10:30 -0600

    Definiendo encabezados de tabla de Empleados

---
+++ src/main/java/com/mx/grupogateway/system/util/IdentificadoresEmpleado.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.mx.grupogateway.system.util;
+
+/**
+ *
+ * @author eduar
+ */
+public enum IdentificadoresEmpleado {
+    ID_EMPLEADO,
+    NOMBRE,
+    APELLIDO_PATERNO,
+    APELLIDO_MATERNO,
+    ID_USUARIO,
+    CARGO
+}
