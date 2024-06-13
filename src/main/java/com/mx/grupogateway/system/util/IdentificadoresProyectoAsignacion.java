131af71aa6b92b2723a1a2c8166e635cc2553e78
eduardo <mcorey316@gmail.com>
2024-06-13 14:09:50 -0600

    Definiendo encabezados de tabla de Asignaciones

---
+++ src/main/java/com/mx/grupogateway/system/util/IdentificadoresProyectoAsignacion.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.mx.grupogateway.system.util;
+
+/**
+ *
+ * @author eduar
+ */
+public enum IdentificadoresProyectoAsignacion {
+    ID_EMPLEADO,
+    NOMBRE,
+    APELLIDO_PATERNO,
+    APELLIDO_MATERNO,
+    FECHA_ASIGNACION,
+    ID_PROYECTO,
+    PO_NO,
+    IMPORTE,
+    TOTAL_PAGAR,
+    STATUS,
+    CUSTOMER,
+    PROJECT_NAME,
+    PO_STATUS,
+    PO_LINE_NO,
+    SITE_CODE,
+    SITE_NAME,
+    ITEM_DESC,
+    REQUESTED_QTY,
+    DUE_QTY,
+    BILLED_QTY,
+    UNIT_PRICE,
+    LINE_AMOUNT,
+    UNIT,
+    PAYMENT_TERMS,
+    CATEGORY,
+    PUBLISH_DATE
+}
