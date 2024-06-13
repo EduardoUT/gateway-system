a405207abba1e59ee757c3eaba48219e384ec8ae
eduardo <mcorey316@gmail.com>
2024-06-13 13:57:24 -0600

    Creando modelo de tabla Empleado

---
+++ src/main/java/com/mx/grupogateway/system/view/util/TableDataModelEmpleado.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mx.grupogateway.system.view.util;
+
+import com.mx.grupogateway.system.modelo.Empleado;
+import com.mx.grupogateway.system.util.IdentificadoresEmpleado;
+import java.util.List;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author eduar
+ */
+public class TableDataModelEmpleado {
+
+    private final DefaultTableModel modeloTabla;
+    private final JTable tabla;
+    private final List<Empleado> empleados;
+
+    public TableDataModelEmpleado(DefaultTableModel modeloTabla, JTable tabla,
+            List<Empleado> empleados) {
+        this.tabla = tabla;
+        this.modeloTabla = modeloTabla;
+        this.empleados = empleados;
+    }
+
+    public void cargarTablaEmpleados() {
+        modeloTabla.getDataVector().clear();
+        tabla.clearSelection();
+        modeloTabla.setColumnIdentifiers(
+                IdentificadoresEmpleado.values()
+        );
+        empleados.forEach(empleado -> {
+            modeloTabla.addRow(
+                    new Object[]{
+                        empleado.getIdEmpleado(),
+                        empleado.getNombre(),
+                        empleado.getApellidoPaterno(),
+                        empleado.getApellidoMaterno(),
+                        empleado.getUsuario().getIdUsuario(),
+                        empleado.getEmpleadoCategoria().getNombreCategoria()
+                    }
+            );
+        });
+    }
+}
