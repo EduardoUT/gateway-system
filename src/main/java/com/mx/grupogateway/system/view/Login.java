5ec48979051bc0aadbfb065471715a21d9544df0
eduardo <mcorey316@gmail.com>
2024-06-25 12:04:32 -0600

    Comentando métodos e implementando lógica de acceso acorde a la categoría del empleado.

--- src/main/java/com/mx/grupogateway/system/view/Login.java
+++ src/main/java/com/mx/grupogateway/system/view/Login.java
@@ -6,9 +6,13 @@ package com.mx.grupogateway.system.view;
 
 import com.formdev.flatlaf.FlatDarkLaf;
 import com.mx.grupogateway.system.controller.UsuarioController;
+import com.mx.grupogateway.system.modelo.Empleado;
+import com.mx.grupogateway.system.modelo.Usuario;
+import com.mx.grupogateway.system.view.util.ValidacionJPasswordField;
 import com.mx.grupogateway.system.view.util.FondoLogin;
+import com.mx.grupogateway.system.view.util.IconoVentana;
 import java.awt.Color;
-import java.util.ArrayList;
+import java.awt.event.MouseEvent;
 import javax.swing.JOptionPane;
 
 /**
@@ -17,37 +21,63 @@ import javax.swing.JOptionPane;
  */
 public class Login extends javax.swing.JFrame {
 
-    private final UsuarioController usuarioController;
-    private ArrayList<String> datosPerfil;
+    private UsuarioController usuarioController;
+    protected Empleado empleado;
 
     /**
      * Creates new form Login
      */
     public Login() {
         initComponents();
+        iniciarProcesos();
+    }
+
+    private void iniciarProcesos() {
+        cargarIconoVentana();
         this.usuarioController = new UsuarioController();
         campoUsuario.requestFocus();
     }
 
-    private void validarCategoria(String idCategoria) {
-        if(idCategoria.equals("2")) {
-            new Facturacion().setVisible(true);
-            this.dispose();
-        }
+    private void cargarIconoVentana() {
+        this.setIconImage(IconoVentana.getIconoVentana());
     }
 
+    /**
+     * Realiza el llamado de la ventana que corresponda a la categoría del
+     * empleado.
+     *
+     * @param idCategoria Identificador de categoría obtenido de la BD.
+     */
+    private void validarCategoria(String idCategoria) {
+        if (idCategoria.equals("1")) {
+            Gestion gestion = new Gestion();
+            gestion.setUsuario(empleado.getUsuario());
+            gestion.setVisible(true);
+        } else {
+            Facturacion facturacion = new Facturacion();
+            facturacion.setUsuario(empleado.getUsuario());
+            facturacion.setVisible(true);
+        }
+        this.dispose();
+    }
+
+    /**
+     * Toma el nombre del usuario y la contraseña para posteriormente,
+     * consultarlos en la BD y de ser validos consultar los identificadores
+     * (Categoría y Usuario) a los que corresponde.
+     */
     private void validarPerfilUsuario() {
         String nombreUsuario = campoUsuario.getText();
-        String password = String.valueOf(campoPassword.getPassword());
-        datosPerfil = this.usuarioController.consultarPerfilUsuario(
-                nombreUsuario, 
-                password
-        );
-        if (datosPerfil.isEmpty()) {
+        char[] password = campoPassword.getPassword();
+        Usuario usuario = new Usuario();
+        usuario.setPassword(String.valueOf(password));
+        usuario.setNombreUsuario(nombreUsuario);
+        empleado = this.usuarioController.consultarPerfilUsuario(usuario);
+        if (empleado == null) {
             JOptionPane.showMessageDialog(null,
                     "El usuario o la contraseña son incorrectos.");
         } else {
-            validarCategoria(datosPerfil.get(1));
+            validarCategoria(empleado.getEmpleadoCategoria().getidCategoria());
         }
     }
 
@@ -68,6 +98,7 @@ public class Login extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jSeparator1 = new javax.swing.JSeparator();
         jSeparator2 = new javax.swing.JSeparator();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Iniciar Sesión");
@@ -84,11 +115,6 @@ public class Login extends javax.swing.JFrame {
         campoPassword.setForeground(new java.awt.Color(0, 0, 0));
         campoPassword.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1), "Contraseña", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 0, 16), new java.awt.Color(0, 0, 0))); // NOI18N
         campoPassword.setCaretColor(new java.awt.Color(0, 0, 0));
-        campoPassword.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                campoPasswordActionPerformed(evt);
-            }
-        });
 
         checkBox.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         checkBox.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/ojoCerrado.png"))); // NOI18N
@@ -116,6 +142,16 @@ public class Login extends javax.swing.JFrame {
         jSeparator2.setBackground(new Color(0, 0, 0, 0));
         jSeparator2.setForeground(new java.awt.Color(0, 0, 0));
 
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 16)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel2.setText("Registrarse");
+        jLabel2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jLabel2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel2MouseClicked(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -131,16 +167,16 @@ public class Login extends javax.swing.JFrame {
                                 .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                         .addGap(0, 434, Short.MAX_VALUE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                    .addComponent(jSeparator2)
-                                    .addComponent(campoPassword, javax.swing.GroupLayout.DEFAULT_SIZE, 360, Short.MAX_VALUE))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(checkBox))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGap(129, 129, 129)
-                                .addComponent(botonIngresar)))
+                                .addComponent(botonIngresar)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jLabel2))
+                            .addComponent(jSeparator2, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(campoPassword, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 360, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(checkBox)
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         jPanel1Layout.setVerticalGroup(
@@ -162,7 +198,9 @@ public class Login extends javax.swing.JFrame {
                         .addGap(13, 13, 13)))
                 .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(botonIngresar)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(botonIngresar)
+                    .addComponent(jLabel2))
                 .addGap(62, 62, 62))
         );
 
@@ -183,21 +221,24 @@ public class Login extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void campoPasswordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_campoPasswordActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_campoPasswordActionPerformed
-
     private void checkBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_checkBoxActionPerformed
-        if (checkBox.isSelected()) {
-            campoPassword.setEchoChar((char) 0);
-        } else {
-            campoPassword.setEchoChar('*');
-        }
+        evt.getID();
+        ValidacionJPasswordField.visualizacionPassword(
+                checkBox, campoPassword
+        );
     }//GEN-LAST:event_checkBoxActionPerformed
 
     private void botonIngresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonIngresarActionPerformed
+        evt.getID();
         validarPerfilUsuario();
     }//GEN-LAST:event_botonIngresarActionPerformed
+
+    private void jLabel2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MouseClicked
+        if (evt.getButton() == MouseEvent.BUTTON1) {
+            new Registro().setVisible(true);
+            this.dispose();
+        }
+    }//GEN-LAST:event_jLabel2MouseClicked
 
     /**
      * @param args the command line arguments
@@ -217,6 +258,7 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JTextField campoUsuario;
     private javax.swing.JCheckBox checkBox;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator2;
