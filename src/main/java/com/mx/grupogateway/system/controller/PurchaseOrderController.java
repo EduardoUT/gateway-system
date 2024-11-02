6a7b8754f2f10ba5bc2fda2115fdb1ba698da13b
eduardo <mcorey316@gmail.com>
2024-11-02 14:15:23 -0600

    Creando clase DAO y Controller para modelo PurchaseOrder

--- src/main/java/com/mx/grupogateway/system/controller/PurchaseOrderController.java
+++ src/main/java/com/mx/grupogateway/system/controller/PurchaseOrderController.java
@@ -4,10 +4,30 @@
  */
 package com.mx.grupogateway.system.controller;
 
+import com.mx.grupogateway.system.dao.PurchaseOrderDAO;
+import com.mx.grupogateway.system.factory.ConnectionFactory;
+import com.mx.grupogateway.system.modelo.PurchaseOrder;
+
 /**
  *
  * @author eduar
  */
 public class PurchaseOrderController {
-    
+
+    private final PurchaseOrderDAO purchaseOrderDAO;
+
+    public PurchaseOrderController() {
+        this.purchaseOrderDAO = new PurchaseOrderDAO(
+                new ConnectionFactory().realizarConexion()
+        );
+    }
+
+    /**
+     * Realiza el guardado de PurchaseOrder.
+     *
+     * @param purchaseOrder
+     */
+    public void guardar(PurchaseOrder purchaseOrder) {
+        this.purchaseOrderDAO.guardar(purchaseOrder);
+    }
 }
--- src/main/java/com/mx/grupogateway/system/dao/PurchaseOrderDAO.java
+++ src/main/java/com/mx/grupogateway/system/dao/PurchaseOrderDAO.java
@@ -4,10 +4,51 @@
  */
 package com.mx.grupogateway.system.dao;
 
+import com.mx.grupogateway.system.modelo.PurchaseOrder;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author eduar
  */
 public class PurchaseOrderDAO {
-    
+
+    private final Connection con;
+
+    public PurchaseOrderDAO(Connection con) {
+        this.con = con;
+    }
+
+    public void guardar(PurchaseOrder purchaseOrder) {
+        String sql = "INSERT INTO PURCHASE_HAS_ORDER "
+                + "(PO_NO, ID_PROJECT, PO_LINE_NO, DUE_QTY, BILLED_QTY, UNIT, "
+                + "UNIT_PRICE, SHIPMENT_NO) "
+                + "VALUES(?,?,?,?,?,?,?,?)";
+        try (PreparedStatement preparedStatement = con.prepareStatement(sql,
+                Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, purchaseOrder.getPurchaseOrderDetail().getPoNo());
+            preparedStatement.setLong(2, purchaseOrder.getProject().getIdProyecto());
+            preparedStatement.setInt(3, purchaseOrder.getPoLineNo());
+            preparedStatement.setString(4, purchaseOrder.getDueQty());
+            preparedStatement.setString(5, purchaseOrder.getBilledQty());
+            preparedStatement.setString(6, purchaseOrder.getUnit());
+            preparedStatement.setBigDecimal(7, purchaseOrder.getUnitPrice());
+            preparedStatement.execute();
+            try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+                while (resultSet.next()) {
+                    System.out.println(String.format("Fue guardado Purchase Order %s", purchaseOrder));
+                }
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(SiteDAO.class.getName()).log(Level.SEVERE, null, e);
+            System.out.println("Error al guardar Purchase Order: " + e.getMessage());
+        }
+    }
+
 }
