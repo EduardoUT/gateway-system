0499c2a7fedf4f71562337d062b01ebb3250571b
eduardo <mcorey316@gmail.com>
2024-12-10 14:36:28 -0600

    Refactorizando métodos para procesar valores genéricos

--- src/main/java/com/mx/grupogateway/system/controller/ExcelController.java
+++ src/main/java/com/mx/grupogateway/system/controller/ExcelController.java
@@ -14,10 +14,11 @@ import com.mx.grupogateway.system.modelo.Site;
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JLabel;
@@ -91,7 +92,7 @@ public class ExcelController extends SwingWorker<Void, Integer> {
                 purchaseOrderDetail.setPoStatus(getCellValueString(row, 10));
                 purchaseOrderDetail.setItemCode(getCellValueLong(row, 17));
                 purchaseOrderDetail.setItemDesc(getCellValueString(row, 18));
-                purchaseOrderDetail.setRequestedQty(getCellValueString(row, 19));
+                purchaseOrderDetail.setRequestedQty(getCellValueBigDecimal(row, 19));
                 purchaseOrderDetail.setLineAmount(getCellValueBigDecimal(row, 23));
                 purchaseOrderDetail.setPaymentTerms(getCellValueString(row, 27));
 
@@ -104,10 +105,10 @@ public class ExcelController extends SwingWorker<Void, Integer> {
                 purchaseOrder.setUnit(getCellValueString(row, 24));
                 purchaseOrder.setUnitPrice(getCellValueBigDecimal(row, 22));
 
-                getDataImport().addSite(site);
-                getDataImport().addProject(project);
-                getDataImport().addPurchaseOrderDetail(purchaseOrderDetail);
-                getDataImport().addPurchaseOrders(purchaseOrder);
+                dataImport.addSite(site);
+                dataImport.addProject(project);
+                dataImport.addPurchaseOrderDetail(purchaseOrderDetail);
+                dataImport.addPurchaseOrders(purchaseOrder);
                 int progress = i * 100 / numLastRow;
                 publish(progress);
             }
@@ -156,6 +157,25 @@ public class ExcelController extends SwingWorker<Void, Integer> {
     }
 
     /**
+     * Método para procesar el tipo de dato genérico según corresponda en la
+     * celda a procesar.
+     *
+     * @param <T>
+     * @param row Fila de la hoja de excel.
+     * @param numCell Número de celda.
+     * @param mapper Función a implementar acorde al tipo de dato a procesar.
+     * @param defaultValue Valor por defecto cuando sea vacío o no válido el
+     * valor.
+     * @return Retorna un genérico acorde a la implementación del tipo de dato.
+     */
+    private <T> T getCellValue(Row row, int numCell, Function<String, T> mapper, T defaultValue) {
+        return Optional.ofNullable(row.getCell(numCell))
+                .map(cell -> cell.toString())
+                .map(mapper)
+                .orElse(defaultValue);
+    }
+
+    /**
      * Obtiene el objeto Row y el número de celda ideal para celdas con dato de
      * tipo texto.
      *
@@ -164,11 +184,7 @@ public class ExcelController extends SwingWorker<Void, Integer> {
      * @return La representación del dato obtenido en un String.
      */
     private String getCellValueString(Row row, int numCell) {
-        String optionalToString;
-        optionalToString = Optional.ofNullable(row.getCell(numCell))
-                .map(Object::toString)
-                .orElse("");
-        return optionalToString;
+        return getCellValue(row, numCell, String::valueOf, "");
     }
 
     /**
@@ -180,15 +196,13 @@ public class ExcelController extends SwingWorker<Void, Integer> {
      * @return La reprecentación del dato obtenido en un Integer.
      */
     private Integer getCellValueInteger(Row row, int numCell) {
-        String optionalToString;
-        optionalToString = Optional.ofNullable(row.getCell(numCell))
-                .map(Object::toString)
-                .orElse("0");
-        if (optionalToString.equals("")) {
-            optionalToString = "0";
-        }
-        Long num = Math.round(Double.parseDouble(optionalToString));
-        return num.intValue();
+        return getCellValue(row, numCell, value -> {
+            if (value.equals("")) {
+                return 0;
+            }
+            Long number = Math.round(Double.parseDouble(value));
+            return number.intValue();
+        }, 0);
     }
 
     /**
@@ -200,15 +214,12 @@ public class ExcelController extends SwingWorker<Void, Integer> {
      * @return La representación del valor obtenido en un Long.
      */
     private Long getCellValueLong(Row row, int numCell) {
-        String optionalToString;
-        optionalToString = Optional.ofNullable(row.getCell(numCell))
-                .map(Object::toString)
-                .orElse("0");
-        if (optionalToString.equals("")) {
-            optionalToString = "0";
-        }
-        return BigDecimal.valueOf(Double.parseDouble(optionalToString))
-                .longValue();
+        return getCellValue(row, numCell, value -> {
+            if (value.equals("")) {
+                return 0L;
+            }
+            return BigDecimal.valueOf(Double.parseDouble(value)).longValue();
+        }, 0L);
     }
 
     /**
@@ -220,31 +231,26 @@ public class ExcelController extends SwingWorker<Void, Integer> {
      * @return La representación del dato obrenido en BigDecimal.
      */
     private BigDecimal getCellValueBigDecimal(Row row, int numCell) {
-        String optionalToString;
-        optionalToString = Optional.ofNullable(row.getCell(numCell))
-                .map(Object::toString)
-                .orElse("0");
-        if (optionalToString.equals("")) {
-            optionalToString = "0";
-        }
-        return new BigDecimal(optionalToString);
+        return getCellValue(row, numCell, BigDecimal::new, BigDecimal.ZERO);
     }
 
     /**
      * Obtiene el objeto Row y el número de celda, ideal para celdas con fechas
-     * en estandar Timestamp.
+     * en formato Timestamp.
      *
      * @param row
      * @param numCell
      * @return La representación de una fecha en un LocalDateTime.
      */
     private LocalDateTime getCellValueTimestamp(Row row, int numCell) {
-        String optionalToString;
-        optionalToString = Optional.
-                ofNullable(row.getCell(numCell).getStringCellValue())
-                .map(Object::toString)
-                .orElse("NULL");
-        return Timestamp.valueOf(optionalToString).toLocalDateTime();
+        return getCellValue(row, numCell, value -> {
+            if (value.matches("\\d{4}-\\d{2}-\\d{2}\\s{1}\\d{2}:\\d{2}:\\d{2}")) {
+                DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                return LocalDateTime.parse(row.getCell(numCell).getStringCellValue(), dateTimeFormatter);
+            } else {
+                return LocalDateTime.now();
+            }
+        }, LocalDateTime.now());
     }
 
     /**
