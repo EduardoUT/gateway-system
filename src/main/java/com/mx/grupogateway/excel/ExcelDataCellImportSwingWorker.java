8c86b248afee0a6ac3015a879b296d1c1c11e1b5
eduardo <mcorey316@gmail.com>
2025-01-24 17:26:31 -0600

    Definiendo métodos correspondientes al proceso de importación y al SwingWorker, separando métodos de formato de celdas en otra clase

--- src/main/java/com/mx/grupogateway/excel/ExcelDataCellImportSwingWorker.java
+++ src/main/java/com/mx/grupogateway/excel/ExcelDataCellImportSwingWorker.java
@@ -5,19 +5,14 @@
 package com.mx.grupogateway.excel;
 
 import com.mx.grupogateway.config.LoggerConfig;
-import com.mx.grupogateway.excel.ExcelDAO;
 import com.mx.grupogateway.project.Project;
 import com.mx.grupogateway.purchaseorder.PurchaseOrder;
 import com.mx.grupogateway.purchaseorder.detail.PurchaseOrderDetail;
 import com.mx.grupogateway.site.Site;
+import static com.mx.grupogateway.util.ExcelDataCellFormatter.*;
 import java.io.File;
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JLabel;
@@ -29,44 +24,47 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
+ * Permite ejecutat en segundo plano el proceso de importación de los datos del
+ * archivo de Excel.
  *
  * @author eduar
  */
-public class ExcelController extends SwingWorker<Void, Integer> {
+class ExcelDataCellImportSwingWorker extends SwingWorker<Void, Integer> {
 
     private static final Logger logger = LoggerConfig.getLogger();
-    private int numRows;
-    private final ExcelDAO excelDAO;
     private final String excelFilePath;
-    private final DataImport dataImport;
+    private final ExcelDataCell excelDataCell;
     private final JProgressBar jProgressBar;
     private final JLabel jLabel;
+    private final ExcelDataCellExportSwingWorker excelDataCellExportSwingWorker;
 
-    public ExcelController(String excelFilePath,
+    public ExcelDataCellImportSwingWorker(String excelFilePath,
             JProgressBar jProgressBar, JLabel jLabel) {
         this.excelFilePath = excelFilePath;
-        this.dataImport = new DataImport();
+        excelDataCell = new ExcelDataCell();
         this.jProgressBar = jProgressBar;
         this.jProgressBar.setStringPainted(true);
         this.jLabel = jLabel;
-        this.excelDAO = new ExcelDAO(
-                dataImport, jProgressBar, jLabel
+        excelDataCellExportSwingWorker = new ExcelDataCellExportSwingWorker(
+                excelDataCell,
+                jProgressBar,
+                jLabel
         );
     }
 
     /**
      * Realiza la importación y distribución de los datos de la hoja de Excel
      * acorde a las clases modelo, mismas que son contenidas en una clase única
-     * con un listado acorde al tipo de Objeto, esta clase se llama dataImport.
+     * con un listado de dipo HashSet acorde al tipo de Objeto, esta clase se llama
+     * ExcelDataCell.
      *
-     * @see DataImport
+     * @see ExcelDataCell
      */
-    private void importData() {
+    private void importDataCellExcel() {
         File file = new File(excelFilePath);
         try (XSSFWorkbook xssfw = new XSSFWorkbook(file)) {
             XSSFSheet sheet = xssfw.getSheetAt(0);
             int numLastRow = sheet.getLastRowNum();
-            numRows = numLastRow;
             for (int i = 1; i <= numLastRow; i++) {
                 Row row = sheet.getRow(i);
 
@@ -104,14 +102,13 @@ public class ExcelController extends SwingWorker<Void, Integer> {
                 purchaseOrder.setUnit(getCellValueString(row, 24));
                 purchaseOrder.setUnitPrice(getCellValueBigDecimal(row, 22));
 
-                dataImport.addSite(site);
-                dataImport.addProject(project);
-                dataImport.addPurchaseOrderDetail(purchaseOrderDetail);
-                dataImport.addPurchaseOrders(purchaseOrder);
+                excelDataCell.addSite(site);
+                excelDataCell.addProject(project);
+                excelDataCell.addPurchaseOrderDetail(purchaseOrderDetail);
+                excelDataCell.addPurchaseOrders(purchaseOrder);
                 int progress = i * 100 / numLastRow;
                 publish(progress);
             }
-
         } catch (InvalidFormatException e) {
             logger.log(Level.SEVERE, "Error al procesar formato de excel: {0}", e.getMessage());
         } catch (IOException e) {
@@ -121,24 +118,9 @@ public class ExcelController extends SwingWorker<Void, Integer> {
         }
     }
 
-    /**
-     * Invoca el SwingWorker encargado de la exportación de la información
-     * almacenada en los listados del DataModel a la Base de Datos.
-     *
-     * @see ExcelDAO
-     */
-    public void exportData() {
-        if (isDone()) {
-            this.excelDAO.execute();
-        } else {
-            logger.log(Level.SEVERE, "El proceso de importación del archivo de "
-                    + "Excel no se completó exitosamente.");
-        }
-    }
-
     @Override
     protected Void doInBackground() throws Exception {
-        importData();
+        importDataCellExcel();
         return null;
     }
 
@@ -153,116 +135,6 @@ public class ExcelController extends SwingWorker<Void, Integer> {
     protected void done() {
         jProgressBar.setValue(0);
         jLabel.setText("Importación de datos finalizada.");
-    }
-
-    /**
-     * Método para procesar el tipo de dato genérico según corresponda en la
-     * celda a procesar.
-     *
-     * @param <T>
-     * @param row Fila de la hoja de excel.
-     * @param numCell Número de celda.
-     * @param mapper Función a implementar acorde al tipo de dato a procesar.
-     * @param defaultValue Valor por defecto cuando sea vacío o no válido el
-     * valor.
-     * @return Retorna un genérico acorde a la implementación del tipo de dato.
-     */
-    private <T> T getCellValue(Row row, int numCell, Function<String, T> mapper, T defaultValue) {
-        return Optional.ofNullable(row.getCell(numCell))
-                .map(cell -> cell.toString())
-                .map(mapper)
-                .orElse(defaultValue);
-    }
-
-    /**
-     * Obtiene el objeto Row y el número de celda ideal para celdas con dato de
-     * tipo texto.
-     *
-     * @param row
-     * @param numCell
-     * @return La representación del dato obtenido en un String.
-     */
-    private String getCellValueString(Row row, int numCell) {
-        return getCellValue(row, numCell, String::valueOf, "");
-    }
-
-    /**
-     * Obtiene el objeto Row y el número de celda, ideal para celdas con datos
-     * numéicos enteros.
-     *
-     * @param row
-     * @param numCell
-     * @return La reprecentación del dato obtenido en un Integer.
-     */
-    private Integer getCellValueInteger(Row row, int numCell) {
-        return getCellValue(row, numCell, value -> {
-            if (value.equals("")) {
-                return 0;
-            }
-            Long number = Math.round(Double.parseDouble(value));
-            return number.intValue();
-        }, 0);
-    }
-
-    /**
-     * Obtiene el objeto Row y el número de celda, ideal para campos con números
-     * grandes.
-     *
-     * @param row
-     * @param numCell
-     * @return La representación del valor obtenido en un Long.
-     */
-    private Long getCellValueLong(Row row, int numCell) {
-        return getCellValue(row, numCell, value -> {
-            if (value.equals("")) {
-                return 0L;
-            }
-            return BigDecimal.valueOf(Double.parseDouble(value)).longValue();
-        }, 0L);
-    }
-
-    /**
-     * Obtiene el objeto Row y el número de celda, ideal para datos de tipo
-     * decimal.
-     *
-     * @param row
-     * @param numCell
-     * @return La representación del dato obrenido en BigDecimal.
-     */
-    private BigDecimal getCellValueBigDecimal(Row row, int numCell) {
-        return getCellValue(row, numCell, BigDecimal::new, BigDecimal.ZERO);
-    }
-
-    /**
-     * Obtiene el objeto Row y el número de celda, ideal para celdas con fechas
-     * en formato Timestamp.
-     *
-     * @param row
-     * @param numCell
-     * @return La representación de una fecha en un LocalDateTime.
-     */
-    private LocalDateTime getCellValueTimestamp(Row row, int numCell) {
-        return getCellValue(row, numCell, value -> {
-            if (value.matches("\\d{4}-\\d{2}-\\d{2}\\s{1}\\d{2}:\\d{2}:\\d{2}")) {
-                DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-                return LocalDateTime.parse(row.getCell(numCell).getStringCellValue(), dateTimeFormatter);
-            } else {
-                return LocalDateTime.now();
-            }
-        }, LocalDateTime.now());
-    }
-
-    /**
-     * @return the numRows
-     */
-    public int getNumRows() {
-        return numRows;
-    }
-
-    /**
-     * @return the dataImport
-     */
-    public DataImport getDataImport() {
-        return dataImport;
+        excelDataCellExportSwingWorker.execute();
     }
 }
