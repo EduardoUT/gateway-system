21585e73b03f52e742d86de75c7dfd952acb624f
eduardo <mcorey316@gmail.com>
2025-02-14 16:31:27 -0600

    Agregando casos de prueba para PurchaseOrder acorde a la nueva clase estática PurchaseOrderBuilder

--- src/test/java/com/mx/grupogateway/purchaseorder/PurchaseOrderTest.java
+++ src/test/java/com/mx/grupogateway/purchaseorder/PurchaseOrderTest.java
@@ -2,10 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
-package com.mx.grupogateway.system.modelo;
+package com.mx.grupogateway.purchaseorder;
 
+import static com.mx.grupogateway.exception.IllegalArgumentExceptionTypeMessage.*;
 import com.mx.grupogateway.purchaseorder.detail.PurchaseOrderDetail;
-import com.mx.grupogateway.purchaseorder.PurchaseOrder;
 import com.mx.grupogateway.project.Project;
 import java.util.HashSet;
 import java.util.Set;
@@ -24,8 +24,77 @@ import org.junit.jupiter.params.provider.MethodSource;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class PurchaseOrderTest {
 
-    private PurchaseOrder purchaseOrder = new PurchaseOrder();
+    private PurchaseOrder purchaseOrder;
     private final Set<PurchaseOrder> hashSet = new HashSet<>();
+
+    @DisplayName("Debería lanzar IllegalArgumentException para cada argumento "
+            + "null en PurchaseOrder.")
+    @Test
+    void testIllegalArgumentExceptionOnPurchaseOrderArguments() {
+        IllegalArgumentException e;
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrder = new PurchaseOrder.PurchaseOrderBuilder()
+                    .withPurchaseOrderDetail(null)
+                    .build();
+        });
+        assertEquals(NULL_VALUE_MESSAGE.toString(), e.getMessage());
+
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrder = new PurchaseOrder.PurchaseOrderBuilder()
+                    .withProject(null)
+                    .build();
+        });
+        assertEquals(NULL_VALUE_MESSAGE.toString(), e.getMessage());
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrder = new PurchaseOrder.PurchaseOrderBuilder()
+                    .withPoLineNo(null)
+                    .build();
+        });
+        assertEquals(NULL_VALUE_MESSAGE.toString(), e.getMessage());
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrder = new PurchaseOrder.PurchaseOrderBuilder()
+                    .withDueQty(null)
+                    .build();
+        });
+        assertEquals(NULL_VALUE_MESSAGE.toString(), e.getMessage());
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrder = new PurchaseOrder.PurchaseOrderBuilder()
+                    .withBilledQty(null)
+                    .build();
+        });
+        assertEquals(NULL_VALUE_MESSAGE.toString(), e.getMessage());
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrder = new PurchaseOrder.PurchaseOrderBuilder()
+                    .withUnit(null)
+                    .build();
+        });
+        assertEquals(NULL_VALUE_MESSAGE.toString(), e.getMessage());
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrder = new PurchaseOrder.PurchaseOrderBuilder()
+                    .withUnitPrice(null)
+                    .build();
+        });
+        assertEquals(NULL_VALUE_MESSAGE.toString(), e.getMessage());
+    }
+
+    @DisplayName("Debería lanzar IllegalArgumentException cuando se ingresa un "
+            + "valor menor o igual a 0 o mayor a Long.MAX_VALUE")
+    @Test
+    void testNumericArgumentBoundaries() {
+        IllegalArgumentException e;
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrder = new PurchaseOrder.PurchaseOrderBuilder()
+                    .withPoLineNo(Integer.MIN_VALUE)
+                    .build();
+        });
+        assertEquals(LESS_THAN_ZERO_OR_MAX_EXCEDED_MESSAGE.toString(), e.getMessage());
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrder = new PurchaseOrder.PurchaseOrderBuilder()
+                    .withPoLineNo(Integer.MAX_VALUE + 1)
+                    .build();
+        });
+        assertEquals(LESS_THAN_ZERO_OR_MAX_EXCEDED_MESSAGE.toString(), e.getMessage());
+    }
 
     @DisplayName("Debería comparar objetos con mismo identificador en Project "
             + "y PurchaseOrderDetail.")
@@ -33,18 +102,22 @@ class PurchaseOrderTest {
     void testPurchaseOrderEquals() {
         PurchaseOrderDetail purchaseOrderDetail = new PurchaseOrderDetail("6081150685-63");
         Project project = new Project(67936906801L);
-        PurchaseOrder purchaseOrderB = new PurchaseOrder();
-        purchaseOrderB.setPurchaseOrderDetail(purchaseOrderDetail);
-        purchaseOrderB.setProject(project);
-        purchaseOrder.setPurchaseOrderDetail(purchaseOrderDetail);
-        purchaseOrder.setProject(project);
+        PurchaseOrder purchaseOrderB = new PurchaseOrder.PurchaseOrderBuilder()
+                .withPurchaseOrderDetail(purchaseOrderDetail)
+                .withProject(project)
+                .build();
+        purchaseOrder = new PurchaseOrder.PurchaseOrderBuilder()
+                .withPurchaseOrderDetail(purchaseOrderDetail)
+                .withProject(project)
+                .build();
         assertTrue(purchaseOrder.equals(purchaseOrderB));
     }
 
     @DisplayName("Deería comparar objetos con la misma referencia")
     @Test
     void testSameReference() {
-        PurchaseOrder purchaseOrderB = new PurchaseOrder();
+        PurchaseOrder purchaseOrderB = new PurchaseOrder.PurchaseOrderBuilder()
+                .build();
         purchaseOrder = purchaseOrderB;
         assertTrue(purchaseOrder.equals(purchaseOrderB));
     }
@@ -67,16 +140,17 @@ class PurchaseOrderTest {
         assertEquals(1, hashSet.size());
     }
 
+    @SuppressWarnings("unused")
     static Stream<PurchaseOrder> purchaseOrderProvider() {
         return Stream.of(
-                new PurchaseOrder(
-                        new PurchaseOrderDetail("6081150685-63"),
-                        new Project(67936906801L)
-                ),
-                new PurchaseOrder(
-                        new PurchaseOrderDetail("6081150685-63"),
-                        new Project(67936906801L)
-                )
+                new PurchaseOrder.PurchaseOrderBuilder()
+                        .withPurchaseOrderDetail(new PurchaseOrderDetail("6081150685-63"))
+                        .withProject(new Project(67936906801L))
+                        .build(),
+                new PurchaseOrder.PurchaseOrderBuilder()
+                        .withPurchaseOrderDetail(new PurchaseOrderDetail("6081150685-63"))
+                        .withProject(new Project(67936906801L))
+                        .build()
         );
     }
 }
