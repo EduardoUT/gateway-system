d6fedadd6fd56281ce1f4a86df30ba564e3d7ac2
eduardo <mcorey316@gmail.com>
2025-02-14 16:33:21 -0600

    Agregando casos de prueba para PurchaseOrderDetail

--- src/test/java/com/mx/grupogateway/purchaseorder/detail/PurchaseOrderDetailTest.java
+++ src/test/java/com/mx/grupogateway/purchaseorder/detail/PurchaseOrderDetailTest.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
-package com.mx.grupogateway.system.modelo;
+package com.mx.grupogateway.purchaseorder.detail;
 
-import com.mx.grupogateway.purchaseorder.detail.PurchaseOrderDetail;
+import static com.mx.grupogateway.exception.IllegalArgumentExceptionTypeMessage.*;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Stream;
@@ -24,6 +24,60 @@ class PurchaseOrderDetailTest {
 
     private PurchaseOrderDetail purchaseOrderDetail = new PurchaseOrderDetail();
     private final Set<PurchaseOrderDetail> hashSet = new HashSet<>();
+
+    @DisplayName("Debería lanzar IllegalArgumentException para argumentos de tipo "
+            + "String cuyo valor sea null o vacío.")
+    @Test
+    void testIllegalArgumentExceptionOnPurchaseOrderDetailArguments() {
+        IllegalArgumentException e;
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrderDetail.setId(null);
+        });
+        assertEquals(NULL_VALUE_OR_EMPTY_MESSAGE.toString(), e.getMessage());
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrderDetail.setId("");
+        });
+        assertEquals(NULL_VALUE_OR_EMPTY_MESSAGE.toString(), e.getMessage());
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrderDetail.setPoStatus(null);
+        });
+        assertEquals(NULL_VALUE_MESSAGE.toString(), e.getMessage());
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrderDetail.setItemCode(null);
+        });
+        assertEquals(NULL_VALUE_MESSAGE.toString(), e.getMessage());
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrderDetail.setItemDesc(null);
+        });
+        assertEquals(NULL_VALUE_MESSAGE.toString(), e.getMessage());
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrderDetail.setRequestedQty(null);
+        });
+        assertEquals(NULL_VALUE_MESSAGE.toString(), e.getMessage());
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrderDetail.setLineAmount(null);
+        });
+        assertEquals(NULL_VALUE_MESSAGE.toString(), e.getMessage());
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrderDetail.setPaymentTerms(null);
+        });
+        assertEquals(NULL_VALUE_MESSAGE.toString(), e.getMessage());
+    }
+
+    @DisplayName("Debería lanzar IllegalArgumentException cuando se ingresa un "
+            + "valor menor a 0 o mayor a Long.MAX_VALUE")
+    @Test
+    void testNumericArgumentsBoundaries() {
+        IllegalArgumentException e;
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrderDetail.setItemCode(Long.MIN_VALUE);
+        });
+        assertEquals(LESS_THAN_ZERO_OR_MAX_EXCEDED_MESSAGE.toString(), e.getMessage());
+        e = assertThrowsExactly(IllegalArgumentException.class, () -> {
+            purchaseOrderDetail.setItemCode(Long.MAX_VALUE + 1);
+        });
+        assertEquals(LESS_THAN_ZERO_OR_MAX_EXCEDED_MESSAGE.toString(), e.getMessage());
+    }
 
     @DisplayName("Debería comparar objetos con mismo purchaseOrderDetailIdentifier.")
     @Test
@@ -57,6 +111,7 @@ class PurchaseOrderDetailTest {
         assertEquals(1, hashSet.size());
     }
 
+    @SuppressWarnings("unused")
     static Stream<PurchaseOrderDetail> purchaseOrderDetailProvider() {
         return Stream.of(
                 new PurchaseOrderDetail("6081153460-21"),
