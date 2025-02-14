f514cc1b0497e08325a88253c71e844437bdcbcb
eduardo <mcorey316@gmail.com>
2025-02-14 14:33:47 -0600

    Invocando registerLoggerSevere y agregando bloque try-catch

--- src/main/java/com/mx/grupogateway/excel/ExcelDataCellImportSwingWorker.java
+++ src/main/java/com/mx/grupogateway/excel/ExcelDataCellImportSwingWorker.java
@@ -4,7 +4,7 @@
  */
 package com.mx.grupogateway.excel;
 
-import com.mx.grupogateway.config.LoggerConfig;
+import static com.mx.grupogateway.GlobalLogger.*;
 import com.mx.grupogateway.project.Project;
 import com.mx.grupogateway.purchaseorder.PurchaseOrder;
 import com.mx.grupogateway.purchaseorder.detail.PurchaseOrderDetail;
@@ -13,8 +13,6 @@ import static com.mx.grupogateway.util.ExcelDataCellFormatter.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JLabel;
 import javax.swing.JProgressBar;
 import javax.swing.SwingWorker;
@@ -31,7 +29,6 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  */
 class ExcelDataCellImportSwingWorker extends SwingWorker<Void, Integer> {
 
-    private static final Logger logger = LoggerConfig.getLogger();
     private final String excelFilePath;
     private final ExcelDataCell excelDataCell;
     private final JProgressBar jProgressBar;
@@ -55,8 +52,8 @@ class ExcelDataCellImportSwingWorker extends SwingWorker<Void, Integer> {
     /**
      * Realiza la importación y distribución de los datos de la hoja de Excel
      * acorde a las clases modelo, mismas que son contenidas en una clase única
-     * con un listado de dipo HashSet acorde al tipo de Objeto, esta clase se llama
-     * ExcelDataCell.
+     * con un listado de dipo HashSet acorde al tipo de Objeto, esta clase se
+     * llama ExcelDataCell.
      *
      * @see ExcelDataCell
      */
@@ -69,7 +66,7 @@ class ExcelDataCellImportSwingWorker extends SwingWorker<Void, Integer> {
                 Row row = sheet.getRow(i);
 
                 Site site = new Site();
-                site.setSiteId(getCellValueLong(row, 6));
+                site.setId(getCellValueLong(row, 6));
                 site.setSiteCode(getCellValueString(row, 14));
                 site.setSiteName(getCellValueString(row, 16));
                 site.setBiddigArea(getCellValueString(row, 37));
@@ -93,15 +90,15 @@ class ExcelDataCellImportSwingWorker extends SwingWorker<Void, Integer> {
                 purchaseOrderDetail.setLineAmount(getCellValueBigDecimal(row, 23));
                 purchaseOrderDetail.setPaymentTerms(getCellValueString(row, 27));
 
-                PurchaseOrder purchaseOrder = new PurchaseOrder();
-                purchaseOrder.setPurchaseOrderDetail(purchaseOrderDetail);
-                purchaseOrder.setProject(project);
-                purchaseOrder.setPoLineNo(getCellValueInteger(row, 12));
-                purchaseOrder.setDueQty(getCellValueBigDecimal(row, 20));
-                purchaseOrder.setBilledQty(getCellValueBigDecimal(row, 21));
-                purchaseOrder.setUnit(getCellValueString(row, 24));
-                purchaseOrder.setUnitPrice(getCellValueBigDecimal(row, 22));
-
+                PurchaseOrder purchaseOrder = new PurchaseOrder.PurchaseOrderBuilder()
+                        .withPurchaseOrderDetail(purchaseOrderDetail)
+                        .withProject(project)
+                        .withPoLineNo(getCellValueInteger(row, 12))
+                        .withDueQty(getCellValueBigDecimal(row, 20))
+                        .withBilledQty(getCellValueBigDecimal(row, 21))
+                        .withUnit(getCellValueString(row, 24))
+                        .withUnitPrice(getCellValueBigDecimal(row, 22))
+                        .build();
                 excelDataCell.addSite(site);
                 excelDataCell.addProject(project);
                 excelDataCell.addPurchaseOrderDetail(purchaseOrderDetail);
@@ -110,17 +107,22 @@ class ExcelDataCellImportSwingWorker extends SwingWorker<Void, Integer> {
                 publish(progress);
             }
         } catch (InvalidFormatException e) {
-            logger.log(Level.SEVERE, "Error al procesar formato de excel: {0}", e.getMessage());
+            registerLoggerSevere("Error al procesar formato de excel: {0}", e);
         } catch (IOException e) {
-            logger.log(Level.SEVERE, "Error de IO: {0}", e.getMessage());
+            registerLoggerSevere("Error de IO: {0}", e);
         } catch (RuntimeException e) {
-            logger.log(Level.SEVERE, "Error: {0}", e.getMessage());
+            registerLoggerSevere("Error: {0}", e);
         }
     }
 
     @Override
     protected Void doInBackground() throws Exception {
-        importDataCellExcel();
+        try {
+            importDataCellExcel();
+        } catch (Exception e) {
+            registerLoggerSevere("Error en el proceso de importación de los "
+                    + "datos de la hoja de Excel: {0}", e);
+        }
         return null;
     }
 
